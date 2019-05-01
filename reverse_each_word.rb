492d1052a01726b73c41964325ace5e959b736b6
James Britton <brittonjp@gmail.com>
2019-05-01 14:07:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,10 @@ def reverse_each_word(sentence)
   reversed_sentence_string = reversed_sentence.join(" ")
   
 end
+
+def reverse_each_word(sentence)
+  word_array = sentence.split
+  word_array.collect { |word| word.reverse }
+  reversed_sentence_string = reversed_sentence.join(" ")
+  
+end
